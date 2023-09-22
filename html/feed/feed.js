5777fee4db4994c54f8f45b06db3c1ecec64e4d4
AnaCascante <ana.cascante@stud.noroff.no>
2023-09-22 06:31:16 +0200

    som js

--- html/feed/feed.js
+++ html/feed/feed.js
@@ -1,16 +1,26 @@
 import { apiUrl, authToken } from './loginFunc.js';
 
 
-function fetchPosts (){
-    const response = await fetch(`${url}/social/auth/register`,{
-        method:"POST",
-        credentials:"same-origin",
-        headers:{
-            "Content-Type":"application/json"
+async function fetchPosts (){
+    try {
+        const token = localStorage.getItem ('acces Token'); 
+        const getData ={
+            method:"GET",
+            credentials:"same-origin",
+            headers:{
+                "Content-Type":"application/json",
+            Authorization: 'Bearer ${token}',
         },
-        body:JSON.stringify(data)
-    
+    };
+  
+const response = await fetch (apiUrl,getData); 
+console.log (response); ¨
+const json= await response.json();
+console.log (json); 
+    } catch (eror){
+        console.log (error);
     }
+}
 
 
 
