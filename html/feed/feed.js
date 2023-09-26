445e97a1964b0a9c4d19952a44972ddb33542df9
AnaCascante <ana.cascante@stud.noroff.no>
2023-09-26 07:45:49 +0200

    my card is not visible in the html?

--- html/feed/feed.js
+++ html/feed/feed.js
@@ -14,7 +14,7 @@ const options ={
 
 async function getPosts (headerOptions){
     try {
-        const response = await fetch (`${baseUrl}/social/posts`,headerOptions); 
+        const response = await fetch (`${baseUrl}/social/posts?_author=true&_comments=true&_reactions=true`,headerOptions); 
         const posts= await response.json();
         console.log (posts); 
         return posts;
@@ -24,3 +24,60 @@ async function getPosts (headerOptions){
 }
 
 getPosts(options);
+
+
+
+/* this is the js from hell part 2 */
+
+/*---- structure from html for th card  --- transform this into js*/
+
+
+async function createCards (){
+        const posts = await getPosts()
+
+      posts.forEach (post => {
+
+      const postContainer = document.createElement ("div"); 
+        Container.className = "container ";
+
+       const row = document.createElement ("div"); 
+        row.className = "row";
+
+
+       const colRow = document.createElement ("div"); 
+       colrow.className = "row justify-content-center align-items-center text-center col";
+
+
+       const card = document.createElement ("div");
+        card.className = "card mb-3"
+        card.style="max-width: 540px";
+
+       const cardRow = document.createElement ("div"); 
+        cardRow.className = "row g-0";
+
+
+        const img = document.createElement ("div");
+        img.className = "col-md-4";
+        const userImg = document.createElement ("img");
+        userImg.src = element.author.avatar?? "../../img/lion2.jpg"; 
+        userImg.className="img-fluid rounded-start";
+        img.appendChild (userImg);
+
+      const col = document.createElement ("div"); 
+       col.className = "col-md-8";
+
+      const cardBody = document.createElement ("div"); 
+       cardBody.className = "card-body";
+
+      const postTitle = document.createElement ("h5"); 
+        postTitle.textContent = post.title;
+        postTitle.className = "card-title";
+
+      const postText = document.createElement ("p"); 
+        postText.textContent = card.Text;
+        postText.className = "card-text";
+
+    
+         })
+        }
+       
