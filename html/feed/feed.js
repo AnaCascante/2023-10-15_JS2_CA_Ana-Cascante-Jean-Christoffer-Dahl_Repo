02b6e9dd335bdce4dd0dd653844ba474904be972
AnaCascante <91411170+AnaCascante@users.noreply.github.com>
2023-09-26 18:48:17 +0200

    working on it...

--- html/feed/feed.js
+++ html/feed/feed.js
@@ -1,4 +1,15 @@
+
+
 const baseUrl = "https://api.noroff.dev/api/v1";
+
+
+/*userName =  localStorage.getItem('name', userName); */
+const cardContainer = document.getElementById ('cardContainer'); 
+
+const cardTitle = document.getElementById ('cardTitle'); 
+const cardText = document.getElementById ('cardText'); 
+const cardImg = document.getElementById ('cardImg'); 
+
 
 let token
 token = localStorage.getItem('bearerToken', token);
@@ -37,7 +48,51 @@ async function createCards (){
 
       posts.forEach (post => {
 
-      const postContainer = document.createElement ("div"); 
+        /* this is just the "body of the card"*/
+  
+
+        const cardContainer = document.createElement ('div')
+        cardContainer.id = element.id; 
+
+        const cardImg = document.createElement ('div'); 
+        const Img = document.createElement ('img'); 
+        Imgsrc= element.media; 
+
+        const cardTitle = document.createElement ('h2'); 
+      
+        const cardBodyText = document.createElement ("p"); 
+        cardText.textContent = card.Text;
+        cardText.className = "card-text";
+      
+
+
+
+console.log (createCards);
+        
+return createCards ();
+
+         })
+
+      
+        }
+       
+    /** in the card- I need  
+     * author.name 
+     * title 
+     * body 
+     * comments 
+     * id 
+     * media 
+     * reaction 
+     * _count.comments 
+     * _count.reaction
+     * 
+     * */ 
+
+/*  dette er != struktur enn profile card struktur- tatt i bruk profil Card struktur. 
+
+
+const postContainer = document.createElement ("div"); 
         Container.className = "container ";
 
        const row = document.createElement ("div"); 
@@ -51,6 +106,7 @@ async function createCards (){
        const card = document.createElement ("div");
         card.className = "card mb-3"
         card.style="max-width: 540px";
+        card.id = element.id;
 
        const cardRow = document.createElement ("div"); 
         cardRow.className = "row g-0";
@@ -58,16 +114,15 @@ async function createCards (){
 
         const img = document.createElement ("div");
         img.className = "col-md-4";
-        const userImg = document.createElement ("img");
-        userImg.src = element.author.avatar?? "../../img/lion2.jpg"; 
-        userImg.className="img-fluid rounded-start";
-        img.appendChild (userImg);
+        const cardImg = document.createElement ("img");
+        cardImg.appendChild (cardImg);
 
       const col = document.createElement ("div"); 
        col.className = "col-md-8";
 
       const cardBody = document.createElement ("div"); 
        cardBody.className = "card-body";
+      
 
       const postTitle = document.createElement ("h5"); 
         postTitle.textContent = post.title;
@@ -75,9 +130,4 @@ async function createCards (){
 
       const postText = document.createElement ("p"); 
         postText.textContent = card.Text;
-        postText.className = "card-text";
-
-    
-         })
-        }
-       
+        postText.className = "card-text";*/
