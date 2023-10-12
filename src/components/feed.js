5cfa5bc28ce052d634289f7b1c72ef5d19e0e5e3
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2023-10-12 11:14:01 +0200

    added jsdoc comments to feed.js

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,19 +1,63 @@
+/**
+ * Import module for getting the timestamp.
+ * @module timeStamp
+ */
 import timeStamp from "../utils/timeStamp.js";
+/**
+ * Import module for posting comments.
+ * @module postComment
+ */
 import postComment from "../utils/postComment.js";
+/**
+ * Import module for reacting to posts.
+ * @module reactToPosts
+ */
 import reactToPosts from "../utils/reactToPosts.js";
+/**
+ * Import module for deleting posts.
+ * @module deletePost
+ */
 import deletePost from "../utils/deletePost.js";
+/**
+ * Import module for editing posts.
+ * @module editPost
+ */
 import editPost from "../utils/editPost.js";
+/**
+ * Import module for getting user profiles.
+ * @module getProfile
+ */
 import getProfile from "../utils/getProfile.js";
+/**
+ * Import module for following other users.
+ * @module follow
+ */
 import follow from "../utils/follow.js";
+/**
+ * Import module for creating posts.
+ * @module createPosts
+ */
 import createPost from "../utils/createPosts.js";
+/**
+ * Import module for filtering posts.
+ * @module filterPost
+ */
 import filterPosts from "../utils/filterPost.js";
+/**
+ * Import module for searching posts.
+ * @module search
+ */
 import searchPosts from "../utils/search.js";
+/** @type {string} */
 let token;
+/** @type {string} */
 const baseURL = "https://api.noroff.dev/api/v1";
+/** @type {string} */
 let userName;
 token = localStorage.getItem("bearerToken", token);
 
 userName = localStorage.getItem("name", userName);
+/** @type {RequestInit} */
 const options = {
   method: "GET",
   credentials: "same-origin",
@@ -23,6 +67,7 @@ const options = {
   },
 };
 
+/** @type {string[]} */
 const cssSelectors =
 [
 "#feed-container",
@@ -36,7 +81,9 @@ const cssSelectors =
 "#createPostForm"
 
 ]
+/** @type {Element[]} */
 const querySelectors = cssSelectors.map(value => document.querySelector(value))
+/** @type {Element[]} */
 const
 [
  feedContainer,
@@ -48,10 +95,15 @@ const
  followersCount,
  followModalButton,
  createPostForm
- 
 
 ] = querySelectors
 
+/**
+ * Fetch posts using the provided header options.
+ *
+ * @param {RequestInit} headerOptions - Request headers and options.
+ * @returns {Promise<any>} - A promise that resolves to the retrieved posts.
+ */
 async function getPosts(headerOptions) {
   try {
     const response = await fetch(
@@ -66,6 +118,9 @@ async function getPosts(headerOptions) {
   }
 }
 
+/**
+ * Generates the page by fetching posts and displaying them in the feed container.
+ */
 async function generatePage() {
   const data = await getPosts(options);
   if (data) {
@@ -73,6 +128,9 @@ async function generatePage() {
   }
 }
 
+/**
+ * Displays a snackbar notification.
+ */
 function showSnackbar() {
   const snackbar = document.getElementById("snackbar");
   snackbar.className = "show";
@@ -80,6 +138,12 @@ function showSnackbar() {
       snackbar.className = snackbar.className.replace("show", "");
   }, 3000); // This will hide the snackbar after 3 seconds
 }
+/**
+ * Event listener for form submission when creating a post.
+ *
+ * @param {Event} e - The form submission event.
+ * @returns {Promise<any>} - A promise that resolves to the response from creating the post.
+ */
 createPostForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   
@@ -102,6 +166,11 @@ createPostForm.addEventListener('submit', async (e) => {
  return response
 });
 
+/**
+ * Event listener for editing a post.
+ *
+ * @param {Event} event - The click event on the edit button.
+ */
 
 confirmEdit.addEventListener('click', async function(event) {
 
@@ -132,6 +201,11 @@ confirmEdit.addEventListener('click', async function(event) {
 
 
 //userModal from hell
+/**
+ * Opens a user profile modal.
+ *
+ * @param {object} element - The post element.
+ */
 async function userModal(element) {
   const userBio = await getProfile(baseURL, element.author?.name, options);
   const modal = new bootstrap.Modal(
@@ -158,12 +232,27 @@ async function userModal(element) {
     followModalButton.removeEventListener("click", followClickListener);
   });
 }
+/**
+ * Event listener for handling the "Follow" button click.
+ *
+ * @param {string} authorName - The name of the author to follow.
+ * @returns {function} - A function to handle the "Follow" button click event.
+ */
 function handleFollowButtonClick(authorName) {
   return function (event) {
     follow(baseURL, authorName, token, event.currentTarget);
   };
 }
 // will be moved to utilsFolder
+/**
+ * Event listener for submitting a comment form.
+ *
+ * @param {Event} e - The form submission event.
+ * @param {object} element - The post element.
+ * @param {HTMLElement} spanComment - The span for comment count.
+ * @param {HTMLElement} commentCountModal - The comment count in the modal.
+ * @param {HTMLElement} commentSectionModal - The comment section in the modal.
+ */
 async function handleFormSubmit(e, element, spanComment, commentCountModal, commentSectionModal) {
   e.preventDefault();
 
@@ -197,6 +286,11 @@ async function handleFormSubmit(e, element, spanComment, commentCountModal, comm
   }
 }
 // will be moved to utilsFolder
+/**
+ * Appends comments to the comment section in the modal.
+ * @param {object[]} comments - An array of comment objects.
+ * @param {HTMLElement} commentSectionModal - The element representing the comment section in the modal.
+ */
 function appendCommentsToModal(comments, commentSectionModal) {
   comments.forEach(comment => {
       const [author, commentElem] = createCommentElement(comment);
@@ -204,6 +298,11 @@ function appendCommentsToModal(comments, commentSectionModal) {
   });
 }
 // will be moved to utilsFolder
+/**
+ * Creates a comment element for display in the modal.
+ * @param {object} comment - The comment object.
+ * @returns {Array<HTMLElement>} An array containing the author and comment elements.
+ */
 function createCommentElement(comment) {
   const commentElem = document.createElement("p");
   const authorElem = document.createElement("h6");
@@ -215,14 +314,23 @@ function createCommentElement(comment) {
 }
 
 //search
+// Event listener for the search input
 document.getElementById("searchInput").addEventListener("input", function() {
+  /**
+   * Handles input changes in the search input field and triggers a search.
+   * @param {Event} event - The input change event.
+   */
   const searchValue = document.getElementById("searchInput").value.toLowerCase();
   searchPosts(searchValue,getPosts,options,feedContainer,generateProfileCards);
 });
 
 //filter
-
+// Event listener for the filter select
 document.getElementById("filterSelect").addEventListener("change", function() {
+  /**
+   * Handles changes in the filter selection and applies the selected filter.
+   * @param {Event} event - The change event of the filter selection.
+   */
   const filterValue = this.value;
   filterPosts(filterValue,feedContainer,generateProfileCards,getPosts,options,userName);
 });
@@ -230,7 +338,12 @@ document.getElementById("filterSelect").addEventListener("change", function() {
 
 
 
-
+/**
+ * Generates profile cards for the posts and appends them to the container.
+ *
+ * @param {object[]} data - An array of post data.
+ * @param {HTMLElement} container - The container element to which profile cards will be appended.
+ */
 function generateProfileCards(data, container) {
   
   data.forEach((element) => {
